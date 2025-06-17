3f97ffb775757754dfdfc1dbb1d7cb01da3835d5
veronikavpb <veronika.vpb@yahoo.com>
2025-06-17 21:00:07 +0200

    ccc

---
+++ .history/front-end/services/UserService_20250617205227.ts
@@ -0,0 +1,17 @@
+import { User } from "@types";
+
+const loginUser = (credentials: { email: string; password: string }) => {
+  return fetch("http://localhost:3000/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+};
+
+const UserService = {
+  loginUser,
+};
+
+export default UserService;
